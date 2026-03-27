14f947173e13b5d6c6b7ad8cfb12835e0795784d
vinicius.lambert <vinicius.lambert@proton.me>
2026-03-27 18:03:33 -0300

    add: index.html and serve it on /

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/viniciusLambert/bootdevCourseBackendGo
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
+	srv := http.Server{
+		Addr:         ":" + port,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+
+		Handler: mux,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
