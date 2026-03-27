9c06ed2a99e160e72a69d06bded4b01d2bba372c
vinicius.lambert <vinicius.lambert@proton.me>
2026-03-27 18:30:16 -0300

    add: helth endpoint

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		_, _ = w.Write([]byte("OK"))
+	})
 
 	srv := http.Server{
 		Addr:         ":" + port,
