426297f1f70905b86ba70c1df5dbecef0d6efdc4
vinicius.lambert <vinicius.lambert@proton.me>
2026-03-29 08:39:13 -0300

    add: database connection

--- apiConfig.go
+++ apiConfig.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"sync/atomic"
+
+	"github.com/viniciusLambert/bootdevCourseBackendGo/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/viniciusLambert/bootdevCourseBackendGo/internal/database"
 )
 
 func main() {
@@ -11,15 +17,17 @@ func main() {
 	const port = "8080"
 
 	apiCfg := apiConfig{}
-	appHandler := http.FileServer(http.Dir(filePathRoot))
+	apiCfg.db = getDatabaseQueries()
 
 	mux := http.NewServeMux()
 
+	appHandler := http.FileServer(http.Dir(filePathRoot))
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(appHandler)))
 	mux.HandleFunc("GET /api/healthz", HandleHealth)
 	mux.HandleFunc("POST /api/validate_chirp", HandleValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.HandleMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.HandleReset)
+
 	srv := http.Server{
 		Addr:         ":" + port,
 		ReadTimeout:  15 * time.Second,
@@ -29,4 +37,14 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func getDatabaseQueries() *database.Queries {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("error connecting to database")
+	}
+	return database.New(db)
 }
